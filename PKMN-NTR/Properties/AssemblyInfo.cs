c249c3acbec1dbd02d748afb3dd20edc03120e19
fa-dx <d.frood@outlook.com>
2016-10-06 09:58:54 +0100

    Changed Pokemon Editing layout. Added Friendship editing. Added Randomize PID button. Added Gender Editing. Renamed Hatched to Egg and reversed functionality.

---
+++ PKMN-NTR/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PKMN NTR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PKMN NTR")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd7cfe42-3900-4148-aa42-6a04af45b783")]
+
+[assembly: AssemblyVersion("1.17.0.0")]
+[assembly: AssemblyFileVersion("1.17.0.0")]
