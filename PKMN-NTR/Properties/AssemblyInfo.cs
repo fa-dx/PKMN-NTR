fbae1d36004b8ba16f0fb052490462a9abc7ce04
drgoku282 <drgokulyrics@gmail.com>
2017-01-23 22:23:29 -0600

    Version 2.0.0

--- PKMN-NTR/Properties/AssemblyInfo.cs
+++ PKMN-NTR/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("cd7cfe42-3900-4148-aa42-6a04af45b783")]
 
-[assembly: AssemblyVersion("1.99.1.6")]
-[assembly: AssemblyFileVersion("1.99.1.6")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
