9a6ddaf8376d3c54de28b8f7bba37f0b9bd4e375
drgoku282 <drgokulyrics@gmail.com>
2017-02-09 17:09:17 -0600

    Move version to 3.0-alpha3

--- PKMN-NTR/Properties/AssemblyInfo.cs
+++ PKMN-NTR/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("cd7cfe42-3900-4148-aa42-6a04af45b783")]
 
 [assembly: AssemblyVersion("3.0.*")]
-[assembly: AssemblyInformationalVersion("3.0-alpha2")]
+[assembly: AssemblyInformationalVersion("3.0-alpha3")]
