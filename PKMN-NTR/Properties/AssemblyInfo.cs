9e5776d209957b58b2cf30819492e5cec3fcf655
drgoku282 <drgokulyrics@gmail.com>
2017-03-11 18:27:00 -0600

    Version 3.1

--- PKMN-NTR/Properties/AssemblyInfo.cs
+++ PKMN-NTR/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("cd7cfe42-3900-4148-aa42-6a04af45b783")]
 
-[assembly: AssemblyVersion("3.0.*")]
-[assembly: AssemblyInformationalVersion("3.0")]
+[assembly: AssemblyVersion("3.1.*")]
+[assembly: AssemblyInformationalVersion("3.1")]
