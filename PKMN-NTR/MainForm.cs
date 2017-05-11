b6a53e897a37cc6b7397a8b167677272d7fea861
fa-dx <fadx@fadx.co.uk>
2017-05-11 17:32:54 +0100

    Added easy modification of the enableillegal bool.
    Added the ability for the end user to easily enable the writing of illegal Pokemon.
    
    Change the name of PKMN-NTR.exe to PKMN-NTR-ILLEGAL.exe to enabled writing of illegal Pokemon. Name the executable to anything else to disable the writing of illegal Pokemon.
    
    You'll be notified on launch if you correctly enabled the writing of illegal Pokemon.

--- PKMN-NTR/MainForm.cs
+++ PKMN-NTR/MainForm.cs
@@ -288,6 +288,8 @@ namespace ntrbase
 
         public MainForm()
         {
+            if (System.AppDomain.CurrentDomain.FriendlyName == "PKMN-NTR-ILLEGAL.exe") { enableillegal = true; WinFormsUtil.Alert("You have enabled writing of illegal Pokemon!"); }
+                    
             Program.ntrClient.DataReady += handleDataReady;
             Program.ntrClient.Connected += connectCheck;
             Program.ntrClient.InfoReady += getGame;
